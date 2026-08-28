7fa946714ca5f089ebc6b925e7ff5cc7f92f3b74
Ivan Glushenkov <glushman966@gmail.com>
2026-08-28 21:26:48 +0300

    update views.py for room and chatroom

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.db import models
+from django.shortcuts import get_object_or_404, render
+
+from chat.models import ChatRoom
 
 
 def chat_page(request, room_name):
+
+    room = get_object_or_404(
+        ChatRoom,
+        name=room_name,
+    )
+
+    if request.user.is_authenticated:
+        rooms = ChatRoom.objects.filter(
+            models.Q(is_private=False)
+            | models.Q(members=request.user)
+        ).distinct()
+    else:
+        rooms = ChatRoom.objects.filter(
+            is_private=False
+        )
+
     return render(
         request,
         "chat/chat.html",
         {
-            "room_name": room_name,
+            "room": room,
+            "room_name": room.name,
+            "rooms": rooms,
         },
     )
