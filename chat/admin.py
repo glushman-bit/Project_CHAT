930c98f9129080f0def96a9f702d73e56272d6cb
Ivan Glushenkov <glushman966@gmail.com>
2026-08-29 16:18:55 +0300

    изменение admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -7,9 +7,9 @@ from .models import Message
 class MessageAdmin(admin.ModelAdmin):
     list_display = (
         "user",
-        "room_name",
+        "room",
         "text",
         "created_at",
     )
-    list_filter = ("room_name", "created_at")
+    list_filter = ("room", "created_at")
     search_fields = ("text", "user__username")
