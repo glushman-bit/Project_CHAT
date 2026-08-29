80361e06f1bd3417dc4515b52c31e0b01f21a775
Ivan Glushenkov <glushman966@gmail.com>
2026-08-29 16:19:57 +0300

    добавление создание комнаты

--- chat/forms.py
+++ chat/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 
 from chat.models import ChatRoom
+from users.forms import User
 
 
 class ChatRoomForm(forms.ModelForm):
+    """Форма создания комнаты."""
 
     class Meta:
         model = ChatRoom
@@ -12,3 +14,42 @@ class ChatRoomForm(forms.ModelForm):
             "description",
             "is_private",
         )
+        labels = {
+            "name": "Название комнаты",
+            "description": "Описание",
+            "is_private": "Приватная комната",
+        }
+        widgets = {
+            "name": forms.TextInput(
+                attrs={
+                    "placeholder": "Введите название комнаты",
+                }
+            ),
+            "description": forms.Textarea(
+                attrs={
+                    "placeholder": "Введите описание комнаты",
+                    "rows": 4,
+                }
+            ),
+        }
+
+
+class AddRoomMemberForm(forms.Form):
+    """Форма добавления пользователя в комнату."""
+
+    user = forms.ModelChoiceField(
+        queryset=User.objects.none(),
+        label="Пользователь",
+    )
+
+    def __init__(self, *args, room=None, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if room is not None:
+            self.fields["user"].queryset = (
+                User.objects
+                .exclude(id=room.owner_id)
+                .exclude(id__in=room.members.values_list("id", flat=True))
+                .order_by("username")
+            )
+
