0cb1570e603866e182c42b6bf8921d612a9a472f
Ivan Glushenkov <glushman966@gmail.com>
2026-08-29 23:50:12 +0300

    создание путей: создание комнаты, редактирования комнаты, добавление участников в комнату, удаление участников из комнаты, выход из комнаты, присоединиться к комнате.

--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,54 @@
 from django.urls import path
 
-from .views import chat_page, CreateRoomView, AddRoomMemberView
+from .views import (
+    AddRoomMemberView,
+    CreateRoomView,
+    LeaveRoomView,
+    RemoveRoomMemberView,
+    UpdateRoomView,
+    chat_page, JoinRoomView,
+)
+
 
 urlpatterns = [
-    path("rooms/create/", CreateRoomView.as_view(), name="create_room"),
-    path("rooms/<int:room_id>/members/", AddRoomMemberView.as_view(), name="add_room_member"),
-    path("<str:room_name>/", chat_page, name="chat"),
+    path(
+        "rooms/create/",
+        CreateRoomView.as_view(),
+        name="create_room",
+    ),
+
+    path(
+        "rooms/<int:room_id>/update/",
+        UpdateRoomView.as_view(),
+        name="update_room",
+    ),
+
+    path(
+        "rooms/<int:room_id>/members/",
+        AddRoomMemberView.as_view(),
+        name="add_room_member",
+    ),
+
+    path(
+        "rooms/<int:room_id>/members/<int:user_id>/remove/",
+        RemoveRoomMemberView.as_view(),
+        name="remove_room_member",
+    ),
+
+    path(
+        "rooms/<int:room_id>/leave/",
+        LeaveRoomView.as_view(),
+        name="leave_room",
+    ),
+
+    path(
+        "<str:room_name>/",
+        chat_page,
+        name="chat",
+    ),
+    path(
+        "rooms/<int:room_id>/join/",
+        JoinRoomView.as_view(),
+        name="join_room",
+),
 ]
