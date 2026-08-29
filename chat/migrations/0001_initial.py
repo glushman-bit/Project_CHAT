f4e206216fad3b8a2d0467d4468c90b23655c10d
Ivan Glushenkov <glushman966@gmail.com>
2026-08-29 16:17:39 +0300

    изменение models Message,

--- chat/migrations/0001_initial.py
+++ chat/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='Message',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('room_name', models.CharField(max_length=100)),
+                ('room', models.CharField(max_length=100)),
                 ('text', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
             ],
