7a571244ccf9dffbf2f3a3e3712887426f5a20c9
Ivan Glushenkov <glushman966@gmail.com>
2026-08-29 19:19:42 +0300

    обновлены пути

--- chat/urls.py
+++ chat/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import chat_page, CreateRoomView, AddRoomMemberView
 
 urlpatterns = [
-    path("create/", CreateRoomView.as_view(), name="create_room"),
-    path("<str:room_name>/members/add/", AddRoomMemberView.as_view(), name="add_room_member"),
+    path("rooms/create/", CreateRoomView.as_view(), name="create_room"),
+    path("rooms/<int:room_id>/members/", AddRoomMemberView.as_view(), name="add_room_member"),
     path("<str:room_name>/", chat_page, name="chat"),
 ]
